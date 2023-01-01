6566939f2f7b6250f3134357ac87c949b07d7ec0
Piyush <impiyush016@gmail.com>
2023-01-01 11:40:47 +0530

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah_temp) {
+  return Math.round((fah_temp - 32)*(5/9)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel_temp) {
+  return Math.round((cel_temp*9/5 + 32)*10)/10;
 };
 
 // Do not edit below this line
