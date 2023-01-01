5546b61630082702fd7ed7673df56b848de3dd42
Piyush <impiyush016@gmail.com>
2023-01-01 11:20:22 +0530

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbers, ...args) {
+    for (let arg of args){
+        idx = numbers.indexOf(arg);
+        if (idx === -1) {
+            continue;
+        }
+        numbers.splice(idx,1);
+    }
+    return numbers;
 
 };
 
