0805928fc7b37eb2dc03958bef51d85224d0756c
Piyush <impiyush016@gmail.com>
2023-01-01 11:30:34 +0530

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum_ = 0;
+    if (!Number.isInteger(a) || !Number.isInteger(b)){
+        return 'ERROR';
+    }
+    if (a < 0 || b < 0){
+        return 'ERROR';
+    }
+    let small = a < b ? a : b ;
+    let large = a + b - small ;
+    for (let i = small; i<= large; i++){
+        sum_ += i;
+    }
+    return sum_;
 };
 
 // Do not edit below this line
