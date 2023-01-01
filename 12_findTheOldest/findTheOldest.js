b695ba794c68646333c3a705af935ea01da308df
Piyush <impiyush016@gmail.com>
2023-01-01 13:05:54 +0530

    Completed 11_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.sort((person1, person2)=>{
+        if (!("yearOfDeath" in person1)){
+            person1.yearOfDeath = (new Date()).getFullYear();
+        }
+        if (!("yearOfDeath" in person2)){
+            person2.yearOfDeath = (new Date()).getFullYear();
+        }
+        let a = person1.yearOfDeath - person1.yearOfBirth;
+        let b = person2.yearOfDeath - person2.yearOfBirth;
+        return a > b ? -1 : 1;
+    })
+    return arr[0];
 };
 
 // Do not edit below this line
