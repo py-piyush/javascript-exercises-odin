a4db12ab22f0914ab014fb5e69b7c17acd2dc864
Piyush <impiyush016@gmail.com>
2023-01-01 11:08:08 +0530

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    res = ''
+    size = s.length
+    for (let i = size - 1; i >= 0; i-- ){
+        res += s[i]
+    }
+    return res
 };
 
 // Do not edit below this line
